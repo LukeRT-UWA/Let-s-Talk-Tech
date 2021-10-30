33ab3bed7cb88af3a4eddec392cd9ce49f47d70d
Luke Thompson <16175583curtin@gmail.com>
2021-10-30 17:10:06 +0800

    add mutations to resolvers - need to test

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,6 +15,20 @@ const resolvers = {
       return Article.findOne({_id: articleId});
     },
   },
+  Mutation: {
+    createCategory: async (parent, args) => {
+      const category = await Category.create(args);
+      return category
+    },
+    createArticle: async (parent, args) => {
+      const article = await Category.create(args)
+      return article
+    },
+    createComment: async (parent, args) => {
+      const comment = await Comment.create(args)
+      return comment
+    },
+  }
 };
 
 module.exports = resolvers;
