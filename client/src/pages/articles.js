3463d6b5c5c1d390e60c603271d2aa7e2167588c
Luke Thompson <16175583curtin@gmail.com>
2021-11-01 20:32:26 +0800

    setup routing

---
+++ client/src/pages/articles.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Articles() {
+  return (
+    <div className = "w-50 mx-auto">
+    </div>
+  );
+}
---
+++ client/src/pages/singleArticle.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function SingleArticle() {
+  return (
+    <div className = "w-50 mx-auto">
+    </div>
+  );
+}
