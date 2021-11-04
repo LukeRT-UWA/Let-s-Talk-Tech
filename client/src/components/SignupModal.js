58392c454016a0aaf949d3623b6fe10346614751
Luke Thompson <16175583curtin@gmail.com>
2021-11-04 22:59:03 +0800

    add sign up modal - need to modify

---
+++ client/src/components/SignupModal.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Modal, Header, Button, Image, Grid, Form, Segment, Divider } from 'semantic-ui-react'
+
+function LoginModal() {
+    const [open, setOpen] = React.useState(false)
+  
+    return (
+      <Modal
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+        open={open}
+        trigger={<Button>Login/Signup</Button>}
+      >
+        <Segment placeholder>
+    <Grid columns={2} relaxed='very' stackable>
+      <Grid.Column>
+        <Form>
+          <Form.Input
+            icon='user'
+            iconPosition='left'
+            label='Username'
+            placeholder='Username'
+          />
+          <Form.Input
+            icon='lock'
+            iconPosition='left'
+            label='Password'
+            type='password'
+          />
+
+          <Button content='Login' primary />
+        </Form>
+      </Grid.Column>
+
+      <Grid.Column verticalAlign='middle'>
+        <Button content='Sign up' icon='signup' size='big' />
+      </Grid.Column>
+    </Grid>
+
+    <Divider vertical>Or</Divider>
+  </Segment>
+      </Modal>
+    )
+  }
+
+export default LoginModal
