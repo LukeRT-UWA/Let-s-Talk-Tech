9c3c02435973adf0ab771d527d3ad87f2fa35c50
Luke Thompson <16175583curtin@gmail.com>
2021-11-06 13:32:35 +0800

    can logout, need to change content depending on logged in status

--- client/src/components/LoginModal.js
+++ client/src/components/LoginModal.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal, Header, Button, Image, Grid, Form, Segment, Divider } from 'semantic-ui-react'
 import SignupModal from "./SignupModal"
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+
 function LoginModal() {
     const [open, setOpen] = React.useState(false)
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const [addUser, { error, data }] = useMutation(LOGIN_USER);
+    
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const { data } = await addUser({
+          variables: { username, password },
+        });
   
+        Auth.login(data.addUser.token);
+        setOpen(false);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    
+    const handleChange = (event) => {
+      const { name, value } = event.target;
+  
+      if (name === 'username') {
+          setUsername(value)
+      }
+      else if (name === 'password') {
+          setPassword(value)
+      }
+    };
+
     return (
       <Modal
         onClose={() => setOpen(false)}
@@ -14,18 +47,24 @@ function LoginModal() {
         <Segment placeholder>
     <Grid columns={2} relaxed='very'g>
       <Grid.Column>
-        <Form>
+        <Form onSubmit={handleFormSubmit}>
           <Form.Input
+            name='username'
             icon='user'
             iconPosition='left'
             label='Username'
             placeholder='Username'
+            value={username}
+            onChange={handleChange}
           />
           <Form.Input
+            name='password'
             icon='lock'
             iconPosition='left'
             label='Password'
             type='password'
+            value={password}
+            onChange={handleChange}
           />
 
           <Button content='Login' primary />
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,9 +2,13 @@ import React from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 import LoginModal from "./LoginModal"
-
+import Auth from '../utils/auth';
 
 const NavBar = () => {
+    const logout = (event) => {
+        event.preventDefault();
+        Auth.logout();
+    };
     return (
       <Menu>
         <Menu.Item name='home'>
@@ -12,9 +16,15 @@ const NavBar = () => {
             <h1 >Let's Talk Tech</h1>
             </Link>
         </Menu.Item>
-        <Menu.Item name='home'>
+        {Auth.loggedIn() ? (
+        <Menu.Item>
+            <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                Logout
+            </button>
+        </Menu.Item>) :
+        (<Menu.Item>
             <LoginModal />
-        </Menu.Item>
+        </Menu.Item>)}
       </Menu>
     )
 }
